dbd8ac201477a6b5037eb6495754070ca46fd8f0
abolybook <abolybook@gmail.com>
2016-05-07 17:18:24 +0800

    v0.3.0+build.1677
    改进：
    
    - 调整了引用《孝经》时的说明，不再直称“孔子说”。
    - 1.4，将“三纲八目”的内容移至正在写的导言中。
    - 3.16，补充了《礼记·射义》的引文。
    - 4.5，增加了“恶乎成名”的辨析，似乎略有新意。
    - 4.15，补充了范纯仁的人生体会，以印证“忠恕”。
    - “子见南子”的经过，还是放在6.28更顺一些。
    - 8.1，补充了“无得而称焉”的解释。
    - 8.6，放宽了“寄百里之命”的解释。
    - 11.18，补充了适当的古注。
    - 14.3、16.1，补充了《后汉书》李固的例子，以印证微行言孙、颠而不扶。
    - 17.19，增加了《日知录》引文，以警空谈之弊。
    - 17.24，改进了“徼”的解释。
    
    修正：
    
    - “中庸”的链接。
    - 15.12的括号嵌套。

--- validate.py
+++ validate.py
@@ -55,10 +55,44 @@ def validate_lylabels(log):
     for i, info in enumerate(labelinfo):
         label = info[0]
         if not label or label[0].isdigit():
-            log.error('Invalid lylabel "%s" on line %d of %s', \
+            log.error('Invalid lylabel "%s" on line %d of %s',
                     label, labelinfo[i][2]+1, infiles[labelinfo[i][1]])
             return False
     return True
+
+
+def validate_lylinks(log):
+    infiles = (
+        'autotopics.tex',
+        'body.tex',
+        'characters.tex',
+        # 'guide.tex',  # TODO: uncomment
+        'license.tex',
+        'preface.tex',
+        'references.tex',
+        'usage.tex'
+    )
+    pat_anchor = re.compile(r'\\lylabel\{(.+?)\}')
+    pat_link = re.compile(r'\\(?:lycharlink|lylink|lyref)\{(.+?)\}')
+
+    anchors = set()
+    for infile in infiles:
+        for lineno, line in enumerate(io.open(infile, encoding=ENCODING)):
+            lineno += 1
+            for x in pat_anchor.finditer(line):
+                anchors.add(x.group(1))
+    blob_labels = set(b[0] for b in rawblobs)
+    anchors |= blob_labels
+
+    valid = True
+    for infile in infiles:
+        for lineno, line in enumerate(io.open(infile, encoding=ENCODING)):
+            for x in pat_link.finditer(line):
+                target = x.group(1)
+                if target not in anchors:
+                    valid = False
+                    log.error('Invalid target: "%s" on line %d of "%s"', target, lineno+1, infile)
+    return valid
 
 
 def validate_lyrefs(log):
@@ -86,25 +120,25 @@ def validate_lyrefs(log):
     for infile in infiles:
         with io.open(infile, encoding=ENCODING) as f:
             lines = f.readlines()
-        for i, line in enumerate(lines):
+        for lineno, line in enumerate(lines):
             if lyref_tag in line:
                 mat = re_lyref_before.search(line)
                 if mat and mat.start() != 0:
-                    log.warning(r'\lyref before may be incorrect on line %d of %s', i+1, infile)
+                    log.warning(r'\lyref before may be incorrect on line %d of %s', lineno+1, infile)
                 mat = re_lyref_after.search(line)
                 if mat and mat.end() != len(line)-1:
-                    log.warning(r'\lyref after may be incorrect on line %d of %s', i+1, infile)
+                    log.warning(r'\lyref after may be incorrect on line %d of %s', lineno+1, infile)
 
                 mat = re_lyref_lyq.search(line)
                 if not mat:
-                    log.error(r'Incorrect \lyref structure on line %d of %s', i+1, infile)
+                    log.error(r'Incorrect \lyref structure on line %d of %s', lineno+1, infile)
                     errors += 1
                 else:
                     label, text = mat.group(1), mat.group(2)
                     try:
                         index = labels.index(label)
                     except ValueError:
-                        log.error(r'Incorrect \lyref label on line %d of %s', i+1, infile)
+                        log.error(r'Incorrect \lyref label on line %d of %s', lineno+1, infile)
                         errors += 1
                     else:
                         if text and text not in special_lyrefs:
@@ -114,44 +148,44 @@ def validate_lyrefs(log):
                             if any(s not in texts[index] for s in snippets):
                                 if '吾不试，故艺' in text:  # special case for quotation mark mismatch
                                     continue
-                                log.error(r'Incorrect \lyref quoted text on line %d of %s', i+1, infile)
+                                log.error(r'Incorrect \lyref quoted text on line %d of %s', lineno+1, infile)
                                 log.error(text)
                                 log.error(texts[index])
                                 errors += 1
     return errors == 0
 
 
-def validate_lylinks(log):
+def validate_lywords(log):
     infiles = (
+        'aboly.tex',
         'autotopics.tex',
         'body.tex',
         'characters.tex',
+        'cover.tex',
+        'coverly.tex',
         # 'guide.tex',  # TODO: uncomment
         'license.tex',
         'preface.tex',
         'references.tex',
-        'usage.tex'
+        'usage.tex',
     )
-    pat_anchor = re.compile(r'\\lylabel\{(.+?)\}')
-    pat_link = re.compile(r'\\ly(?:char)?link\{(.+?)\}')
-
-    anchors = set()
-    for infile in infiles:
-        for lineno, line in enumerate(io.open(infile, encoding=ENCODING)):
-            lineno += 1
-            for x in pat_anchor.finditer(line):
-                anchors.add(x.group(1))
-    blob_labels = set(b[0] for b in rawblobs)
-    anchors |= blob_labels
+    lywords = set(('lyceum', 'lychee', 'lye', 'lying', 'lying-in', 'lyings-in',
+        'lymph', 'lymphatic', 'lymphocyte', 'lymphoid', 'lymphoma', 'lynch',
+        'lyncher', 'lynching', 'lynx', 'lynx-eyed', 'lyre', 'lyric', 'lyrical',
+        'lyrically', 'lyricism', 'lyricist', 'lyrics',
+        'lyitemize', 'lyenumerate', 'lyquotepoem', 'lyquotepoeme', 'lyquotepara', 'lytextbackground'))  # custom words
+    re_lycmd = re.compile(r'\b(ly[a-z]*)', re.I)
 
     valid = True
     for infile in infiles:
         for lineno, line in enumerate(io.open(infile, encoding=ENCODING)):
-            for x in pat_link.finditer(line):
-                target = x.group(1)
-                if target not in anchors:
+            for mat in re_lycmd.finditer(line):
+                if mat.start() > 0 and line[mat.start() - 1] == '\\':
+                    continue
+                word = mat.group(1).lower()
+                if word not in lywords:  # TODO: consider plurals
                     valid = False
-                    log.error('Invalid target: "%s" on line %d of "%s"', target, lineno, infile)
+                    log.error('Invalid lycommand: "%s" on line %d, column %d of "%s"', word, lineno+1, mat.start()+1, infile)
     return valid
 
 
@@ -177,7 +211,7 @@ def validate_pinyin(log):
     for infile in infiles:
         with io.open(infile, encoding=ENCODING) as f:
             lines = f.readlines()
-        for i, line in enumerate(lines):
+        for lineno, line in enumerate(lines):
             for tc in tone_chars:
                 index = 0
                 while True:
@@ -193,11 +227,11 @@ def validate_pinyin(log):
                         if right == -1:
                             valid = False
                         else:
-                            pinyin = line[left + prefix_len : right]
+                            pinyin = line[left + prefix_len: right]
                             valid = all(c in pinyin_chars for c in pinyin)
                     if not valid:
                         if not any(sp in line for sp in special_pinyin):
-                            log.error('Incorrect pinyin on line %d, column %d of %s', i+1, index+1, infile)
+                            log.error('Incorrect pinyin on line %d, column %d of %s', lineno+1, index+1, infile)
                             return False
                         break
                     else:
@@ -208,7 +242,8 @@ def validate_pinyin(log):
 # All above.
 validators = (
     validate_lylabels,
-    validate_lyrefs,
     validate_lylinks,
+    validate_lyrefs,
+    validate_lywords,
     validate_pinyin,
 )
